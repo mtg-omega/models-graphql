7a85930b89a576a5e65976460abeee687f4ff295
Zweer <niccolo@olivieriachille.com>
2017-03-27 13:45:28 +0200

    Made it a lambda serverless function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
-import { GraphQLObjectType, GraphQLSchema } from 'graphql';
+import { graphql, GraphQLObjectType, GraphQLSchema } from 'graphql';
+import { log } from 'zweer-utils';
 
 const queryFields = {};
 
@@ -22,3 +23,28 @@ const Schema = new GraphQLSchema({
 });
 
 export default Schema;
+
+export function handler(event, context, done) {
+  log.debug(event);
+
+  let query = event.query;
+
+  // patch to allow queries from GraphiQL
+  // like the initial introspectionQuery
+  if (event.query && event.query.hasOwnProperty('query')) {
+    query = event.query.query.replace('\n', ' ', 'g');
+  }
+
+  graphql(Schema, query)
+    .then((result) => {
+      if (result.errors) {
+        log.error('Error while executing the graphql query');
+        log.info(result.errors);
+
+        done(result.errors);
+        return;
+      }
+
+      done(null, result.data);
+    });
+}
